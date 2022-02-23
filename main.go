94587d7ae96ac17013a832cdc5b490600eb80b93
Meiras74 <97518918+Meiras74@users.noreply.github.com>
2022-02-23 10:40:01 +0000

    Add files via upload

--- main.go
+++ main.go
@@ -5,15 +5,46 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/net/websocket"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 
-	http.HandleFunc("/", helloHandler)
+	fmt.Println("server start in port : " + port)
+
+	http.Handle("/", websocket.Handler(Echo))
+
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello")
+func Echo(ws *websocket.Conn) {
+
+	for {
+		var reply string
+		err := websocket.Message.Receive(ws, &reply)
+		if err != nil {
+			fmt.Println("Error receive : " + err.Error())
+			break
+		}
+
+		reply = "Echo from server " + reply
+
+		err = websocket.Message.Send(ws, reply)
+		if err != nil {
+			fmt.Println("Can't send")
+		}
+	}
 }
+
+/*func main() {
+	port := os.Getenv("PORT")
+
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}*/
+
+/*func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
+}*/
