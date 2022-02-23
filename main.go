96db928161bebc002b393a535eb15dd6ecf7dae9
Meiras74 <97518918+Meiras74@users.noreply.github.com>
2022-02-23 10:06:59 +0000

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello")
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
 }
