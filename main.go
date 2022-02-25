749ee1b11754fc8de9279435514bf75861ebcb59
Meiras74 <97518918+Meiras74@users.noreply.github.com>
2022-02-25 11:12:05 +0000

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -11,14 +10,14 @@ import (
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		fmt.Println(r.Header.Get("Origin"))
+		log.Print(r.Header.Get("Origin"))
 		if ValidateAddress(r.Header.Get("Origin")) != true {
 			return false
 		}
 		return true
 	},
 }
-var addressAut [2]string = [2]string{"https://meiras.outsystemscloud.com"}
+var addressAut [2]string = [2]string{"https://meiras.outsystemscloud.com", "https://www.piesocket.com"}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -38,13 +37,16 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
+	log.Print(conn)
 	// The event loop
 	for {
+
 		messageType, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("Error during message reading:", err)
 			break
 		}
+
 		log.Printf("Received: %s", message)
 		err = conn.WriteMessage(messageType, message)
 		if err != nil {
